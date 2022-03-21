afb392910f0608b8f6548482d698a4d7fe651a9c
Rafly Rahmat Dani <raflyrdani@gmail.com>
2022-03-21 17:56:36 +0700

    add project

---
+++ 13_KomponenVue/project/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="App">
+        <ol>
+            <li v-for="todo in todos" :key="todo.id">
+                {{ todo }}
+            </li>
+        </ol>
+        <input type="text" v-model="newTodo">
+        <button v-on:click="addTodo">Tambahkan</button>
+        <p v-if="todos.length >= 4">Hebat!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return{
+      NewTodo: "",
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push(this.newTodo);
+       this.newTodo = "";
+    },
+  },
+};
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
